783278a23c5d482e9190ce3378c6fb524b2fd200
bluefrog845 <bluefrog845@gmail.com>
2022-10-10 23:33:06 -0400

    Update some notes after new install

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# dotfiles
+# To install
+Install ansible (2.10+?)
+https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html
+sudo apt-get install libevent-dev ncurses-dev
+./configure.sh
+
--- configure.sh
+++ configure.sh
@@ -1,1 +1,2 @@
-ansible-playbook Setup.yml --extra-vars "TMUX_VER=3.2a" --extra-vars "base_path=~/Desktop/dotfiles"
+mkdir ./vimwiki
+ansible-playbook Setup.yml --extra-vars "TMUX_VER=3.3a" --extra-vars "base_path=~/Desktop/dotfiles"
